1cf4e081feb6cafc55ec1a47d0790681528c2ab4
LiuQian <56627304+Rookie1001hub@users.noreply.github.com>
2025-03-26 20:03:26 +0800

    增加GamePlay工具窗口

---
+++ Assets/AAAGame/GamePlay/Scripts/Editor/GamePlayTools/GamePlayTools.cs
@@ -0,0 +1,31 @@
+﻿using UGF.EditorTools;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[EditorToolMenu("GamePlay/GamePlay", null, 6, true)]
+public class GamePlayTools : EditorToolBase
+{
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
+
+    static readonly string ToolTitle="GamePlayTools";
+    public override string ToolName => ToolTitle;
+
+    public override Vector2Int WinSize => new Vector2Int(600, 800);
+
+   
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
+
+        // Instantiate UXML
+        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+        root.Add(labelFromUXML);
+    }
+}
